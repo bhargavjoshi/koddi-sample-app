39ce9e5ea45e4001e36056cabfd95bf63f8748b4
Bhargav Joshi <bhargavdjoshi@gmail.com>
2015-01-12 19:54:36 +0530

    Register propelServiceProvider with application

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Propel\Silex\PropelServiceProvider(), array(
+    'propel.config_file' => __DIR__.'../config/propel/config.php'
+));
 
 $app['debug'] = true;
 
