82b64d5beb48a656388d6f42cfe0ac34bdc471b8
Bhargav Joshi <bhargavdjoshi@gmail.com>
2015-01-13 10:24:39 +0530

    Add twig-bridge to the project

--- web/index.php
+++ web/index.php
@@ -5,12 +5,14 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Propel\Silex\PropelServiceProvider(), array(
-    'propel.config_file' => __DIR__.'../config/propel/config.php'
+    'propel.config_file' => __DIR__.'/../config/propel/config.php'
 ));
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'../app/views',
+    'twig.path' => __DIR__.'/../app/views',
 ));
+
+$app->register(new Silex\Provider\SessionServiceProvider());
 
 $app['debug'] = true;
 
